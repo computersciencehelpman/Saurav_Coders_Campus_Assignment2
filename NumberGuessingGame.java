9ba16a8887752f27cdd4184a13c02d1d62158011
computersciencehelpman <55709609+computersciencehelpman@users.noreply.github.com>
2023-11-21 13:36:23 +0545

    Number Guessing Game Second Commit

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -7,47 +7,48 @@ public class NumberGuessingGame {
 	
 	public static void main(String[] args) {
 		
+		int numGuesses = 4; // There are 5 guesses but Java has a 0 base index
+		int userGuess;
+		
 		Random rand = new Random();
 		Scanner scan = new Scanner(System.in);
 		
-		int numGuesses = 5;
-		int theRandomNumber = rand.nextInt(100) +1;
+		int theRandomNumber = rand.nextInt(100) +1; //The computer chooses a number between 1 and 100
+		
+		
 		
 		System.out.println("Enter a number between 1 and 100: ");
-		int userGuess = scan.nextInt();
-		
-		
-		
+		userGuess = scan.nextInt();
 		
 		while(numGuesses > 0) {
 			
-			if(userGuess<1 || userGuess>100) {
-				System.out.println("Your guess is not between 1 and 100, please try again: ");
-				userGuess = scan.nextInt();
-			}
-			if(userGuess != theRandomNumber) {
-				System.out.println("You have " +(numGuesses-1)+" guesses left.");
-			}
-			
-			if(userGuess < theRandomNumber) {
-				System.out.println("Please pick a higher number: ");
-				userGuess = scan.nextInt();
-				numGuesses--;
-			}
-			else if(userGuess>theRandomNumber) {
-				System.out.println("Please pick a lower number: ");
-				userGuess = scan.nextInt();
-				numGuesses--;
-			}
-			else if(userGuess == theRandomNumber){
+			if(userGuess == theRandomNumber) { //The user wins if they guess the right number
 				System.out.println("You win!");
 				break;
 			}
 			
-			if(numGuesses ==1){
-				System.out.println("You have 0 guesses left.");
+			if(userGuess <1 || userGuess > 100) { //check if the guess is within bounds
+				System.out.println("Your guess is not between 1 and 100, please try again: ");
+				userGuess = scan.nextInt();
+
+			}
+			
+			if(userGuess > theRandomNumber) {
+				numGuesses--;
+				System.out.println("You have "+(numGuesses+1)+" guesses left.");
+				System.out.println("Please pick a lower number");
+				userGuess = scan.nextInt();
+			}
+			
+			if(userGuess < theRandomNumber) {
+				numGuesses--;
+				System.out.println("You have "+(numGuesses+1)+" guesses left.");
+				System.out.println("Please pick a higher number");
+				userGuess = scan.nextInt();
+			}
+			if(numGuesses == 0) {
+				System.out.println("You have 0 guesses left.");				
 				System.out.println("You lose, the number to guess was "+theRandomNumber);
-				break;
 			}
 			
 		}
@@ -56,3 +57,4 @@ public class NumberGuessingGame {
 	}
 
 }
+
